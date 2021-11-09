2b9084725939a9903d5ee6b3e71cc3ea017cb03b
ChaseRundall <chase.rundall@grinnell-k12.org>
2021-11-09 18:56:14 +0000

    i;joi;joi

--- template-python-flask/flaskr/__init__.py
+++ template-python-flask/flaskr/__init__.py
@@ -53,7 +53,7 @@ def create_app(test_config=None):
     #######################################################################
     #This function is the way you will change your score
     #The texts are going to be the text that will be shown in the next screen
-    def button_clicking(intro_text, a_text,b_text,c_text, print_message_for_debug):
+    def button_clicking(intro_text, a_text,b_text,c_text, d_text, print_message_for_debug):
         print("##################################")
         if request.method == "POST":
             print("You sent a post request")
@@ -77,6 +77,7 @@ def create_app(test_config=None):
             session['choice_a_text'] = a_text
             session['choice_b_text'] = b_text
             session['choice_c_text'] = c_text
+            session['chocie_d_text'] = d_text
             print('message: ', print_message_for_debug)
             print('new score: ', session['score'])
             
@@ -90,49 +91,75 @@ def create_app(test_config=None):
     #1st Question function
     @app.route("/classic_mode_q1",methods=['GET', 'POST'])
     def first_question():
-        session['intro_text'] = "You fell asleep in the library and you suddenly find yourself late to Miss Misa's class! What are you going to do!?"
-        session['choice_a_text'] = 'Start running to class'
-        session['choice_b_text'] = 'Keep sleeping'
-        session['choice_c_text'] = 'Roam the hallway'
+        session['intro_text'] = "Question 1, What Is Your Favorite Food?"
+        session['choice_a_text'] = 'Pizza'
+        session['choice_b_text'] = 'PB&J'
+        session['choice_c_text'] = 'Macaroni & Chesse'
+        session['choice_d_text'] = 'Other'
+    
+
         message = 'Classic Mode was selected'
         next_page = '/classic_mode_q2'
 
-        button_clicking(session['intro_text'], session['choice_a_text'], session['choice_b_text'], session['choice_c_text'], message)
+        button_clicking(session['intro_text'], session['choice_a_text'], session['choice_b_text'], session['choice_c_text'], session['choice_d_text'], message)
         
-        return render_template('classic_mode.html', intro = session['intro_text'], a_text = session['choice_a_text'], b_text = session['choice_b_text'], c_text = session['choice_c_text'], pg_u_goto_after_clicked = next_page) 
+        return render_template('classic_mode.html', intro = session['intro_text'], a_text = session['choice_a_text'], b_text = session['choice_b_text'], c_text = session['choice_c_text'], d_text = session['choice_d_text'], pg_u_goto_after_clicked = next_page) 
 
     #######################################################################
     #2nd Question function
     @app.route("/classic_mode_q2", methods=['GET','POST'])    
     def second_question():
 
-        new_intro_text =  "Mr. Seney sees you running in the hallway..!!!."
-        new_a_text = "Speed up! Hope he doesn't catch me!!"
-        new_b_text = 'Stop... slow down and walk'
-        new_c_text = 'Wave and smile as you fast walk past him.'
+        new_intro_text =  "Question 2, What Is Your Favorite Amusment Park Ride "
+        new_a_text = "Roller Coaster!"
+        new_b_text = 'Ferris Wheel'
+        new_c_text = 'I do not like amusment parks.'
+        new_d_text = 'asf;adf;jas;'
+
         message = 'User just answered Q1'
         next_page = '/classic_mode_q3'
 
-        button_clicking(new_intro_text, new_a_text, new_b_text, new_c_text, message)
-        return render_template('classic_mode.html', intro = session['intro_text'], a_text = session['choice_a_text'], b_text = session['choice_b_text'], c_text = session['choice_c_text'], pg_u_goto_after_clicked = next_page ) 
+        button_clicking(new_intro_text, new_a_text, new_b_text, new_c_text, new_d_text, message)
+        return render_template('classic_mode.html', intro = session['intro_text'], a_text = session['choice_a_text'], b_text = session['choice_b_text'], c_text = session['choice_c_text'], d_text = session['choice_d_text'], pg_u_goto_after_clicked = next_page ) 
       
     #######################################################################
     #3rd Question function
     @app.route("/classic_mode_q3", methods=['GET','POST'])
     def third_question():
 
-        new_intro_text =  'Your friends call you asking where you are at...'
-        new_a_text = "You tell them to not worry about it!"
-        new_b_text = 'Slowly start walking to class'
-        new_c_text = 'Try to meet them in the lunch room'
+        new_intro_text =  'Question 3, What Is Your Favorite Way To Hide A Dead Body'
+        new_a_text = "In The Sewers"
+        new_b_text = 'In A Lake'
+        new_c_text = 'In The Forest'
+        new_d_text = 'Behind Your House Next To The Maple Tree That Is 76 Feet Tall And 6 Inches. Then To Bury An Animal 3 Feet Down, Then Hide The Body 3 Feet Under.'
         message = 'User just answered Q2'
-        next_page = '/classic_mode_q4'
+        next_page = '/end_screen'
 
         
-        button_clicking(new_intro_text, new_a_text, new_b_text, new_c_text, message)
-        return render_template('classic_mode.html', intro = session['intro_text'], a_text = session['choice_a_text'], b_text = session['choice_b_text'], c_text = session['choice_c_text'], pg_u_goto_after_clicked = next_page) 
+        button_clicking(new_intro_text, new_a_text, new_b_text, new_c_text, new_d_text, message)
+        return render_template('classic_mode.html', intro = session['intro_text'], a_text = session['choice_a_text'], b_text = session['choice_b_text'], c_text = session['choice_c_text'], d_text = session['choice_d_text'], pg_u_goto_after_clicked = next_page) 
+        #######################################################################
+    #Classic Mode End Screen
+    @app.route("/end_screen/", methods=['POST'])
+    def ending():
+        button_clicking('', '', '', '', 'Answered Q5 and below will be the final score')  
+        score = session['score']
+        print('Final score: ', score)
+        if score < 6:
+            last_scene = render_template('end_screen.html', ending_text = 'Sorry, you got in trouble anyway...')
+        elif score == 7 or score == 9 or score == 11:
+            last_scene = render_template('end_screen.html', ending_text = 'I have no idea but you escaped!!!!')
+        else:
+            last_scene = render_template('end_screen.html', ending_text = "Uhhh.. You just got yourself suspended...")
 
-    #######################################################################
+        print("##################################")
+        return last_scene
+    sess = Session()
+    sess.init_app(app)
+
+    return app
+
+'''    #######################################################################
     #4th Question function
     @app.route("/classic_mode_q4", methods=['GET','POST'])
     def fourth_question():
@@ -179,7 +206,7 @@ def create_app(test_config=None):
             last_scene = render_template('end_screen.html', ending_text = "Uhhh.. You just got yourself suspended...")
 
         print("##################################")
-        return last_scene
+        return last_scene'''
 #######################################################################
 #Classic mode ended
 #######################################################################
@@ -188,17 +215,6 @@ def create_app(test_config=None):
 
 #######################################################################
 #Create session & run the application
-    sess = Session()
-    sess.init_app(app)
-
-    return app
-
-
-
-
-
-
-
 
 #helpful websites
 #https://stackoverflow.com/questions/15557392/how-do-i-display-images-from-google-drive-on-a-website
